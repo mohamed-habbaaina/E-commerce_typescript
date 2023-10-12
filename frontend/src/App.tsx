eda2a1dbf044a41b05ec614d28d5c47359d91e7f
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-10-12 16:51:12 +0200

    testing view testing data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+// import reactLogo from './assets/react.svg';
+// import viteLogo from '/vite.svg';
+import './App.css';
+import { sampleProducts } from './data';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div>
-      <header>Header</header>
-      <main></main>
-      <footer>Footer</footer>
-    </div>
+      <div>
+        <header>Header</header>
+        <main>
+          <ul>
+            {sampleProducts.map((product) => (
+              <li key={product.slug}>
+                <img src={product.image} alt={product.name} />
+                <h2>{product.name}</h2>
+                <p>{product.price} Euro</p>
+              </li>
+            ))}
+          </ul>
+        </main>
+        <footer>Footer</footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
